f17eed2fb588c2785247ec43801e274873cd8a28
LawrentChen <Laurant.Chen@gmail.com>
2020-06-27 21:05:16 +0800

    add setup for package distribute

---
+++ setup.py
@@ -0,0 +1,28 @@
+# !usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pandas_upsert_to_mysql",
+    version="0.0.1",
+    author="LawrentChen",
+    author_email="laurant.chen@gmail.com",
+    description="Enhanced `to_sql` method in pandas DataFrame, for MySQL database only.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LawrentChen/pandas_upsert_to_mysql",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+if __name__ == '__main__':
+    pass
